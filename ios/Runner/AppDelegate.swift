c1ca3ade497b8dfbb90e5d13494c3f0c03d6a39e
Levin <levin@macbook-air.local>
2023-12-30 14:22:30 +0100

    Working simple ios playback

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,11 @@
 import UIKit
 import Flutter
+import AVFoundation
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  var audioPlayer: AVAudioPlayer!
+  
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
     let audio_method_channel = FlutterMethodChannel(name: "audio_method_channel", binaryMessenger: controller.binaryMessenger)
@@ -15,16 +18,64 @@ import Flutter
       }
     })
     
+    guard
+      let url = Bundle.main.url(forResource: "Limbo", withExtension: "mp3")
+    else {
+      print("Error opening audio file")
+      return false
+    }
+    
+    do {
+      try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, options: AVAudioSession.CategoryOptions.mixWithOthers)
+      try AVAudioSession.sharedInstance().setActive(true)
+    } catch {
+      print("Error setting up AVAudioSession: \(error.localizedDescription)")
+      return false
+    }
+    
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: url)
+    } catch {
+      print("Error setting up AVAudioPlayer: \(error.localizedDescription)")
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
   
-  private func startPlayback(result: FlutterResult) {
+  private func startPlayback (result: FlutterResult) {
+    audioPlayer.play()
+    /*
+     
+     do {
+     audioFile = try AVAudioFile(forReading: url)
+     } catch {
+     print(error.localizedDescription)
+     return
+     }
+     
+     let audioEngine: AVAudioEngine = AVAudioEngine()
+     let audioPlayerNode: AVAudioPlayerNode = AVAudioPlayerNode()
+     
+     audioEngine.attach(audioPlayerNode)
+     audioEngine.connect(audioPlayerNode, to: audioEngine.outputNode, format: audioFile.processingFormat)
+     
+     audioPlayerNode.scheduleFile(audioFile, at: nil)
+     
+     do {
+     try audioEngine.start()
+     audioPlayerNode.play()
+     } catch {
+     print(error.localizedDescription)
+     }*/
+    
+    
     
     result("started")
   }
   
   private func stopPlayback(result: FlutterResult) {
+    audioPlayer.pause()
     result("stopped")
   }
 }
